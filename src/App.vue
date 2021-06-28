bbb226d050107860e51ffc7d329213702cf95852
Leandro Torres <49099782+LeandroTorres26@users.noreply.github.com>
2021-06-28 05:31:37 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  vertical-align: baseline;
+  list-style: none;
+  text-decoration: none;
+  box-sizing: border-box;
+}
+body {
+  background-color: hsl(0, 0%, 90%);
+}
+#app {
+  font-family: "Roboto", sans-serif;
+  text-align: center;
+  color: hsl(0, 0%, 10%);
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>roupas</li>
+        <li>roupas</li>
+        <li>roupas</li>
+        <li>roupas</li>
+        <li>roupas</li>
+      </ul>
+    </nav>
+    <img :src="img" class="logo" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      img: require("../assets/logo.svg"),
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "../assets/_helpers";
+header {
+  background-color: hsl(0, 0%, 100%);
+}
+nav {
+  display: none;
+}
+.logo {
+  width: 10rem;
+}
+</style>
