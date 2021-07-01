a0af498bb8511d8c001aa20979fce6fe623c8220
Leandro Torres <49099782+LeandroTorres26@users.noreply.github.com>
2021-07-01 16:49:57 -0300

    Nav own component and slider animation in menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,51 +1,45 @@
 <template>
   <header>
-    <fa icon="bars" class="menu" @click="hidden = false" />
-    <nav v-if="!hidden">
-      <fa icon="times" class="menu close" @click="hidden = true" />
-      <ul>
-        <li>
-          <a href="#">Jackets<fa icon="chevron-right" /></a>
-        </li>
-        <li>
-          <a href="#">Sneakers<fa icon="chevron-right" /></a>
-        </li>
-        <li>
-          <a href="#">Hoods<fa icon="chevron-right" /></a>
-        </li>
-        <li>
-          <a href="#">Hoods lorem<fa icon="chevron-right" /></a>
-        </li>
-        <li>
-          <a href="#">Hoods<fa icon="chevron-right" /></a>
-        </li>
-        <li>
-          <a href="#">Hoods<fa icon="chevron-right" /></a>
-        </li>
-      </ul>
-      <div class="social-media">
-        <a href="#"><fa :icon="['fab', 'instagram']" /></a>
-        <a href="#"><fa :icon="['fab', 'facebook']" /></a>
-        <a href="#"><fa :icon="['fab', 'twitter']" /></a>
-      </div>
-    </nav>
+    <fa :icon="icon" class="menu" @click="closeMenu()" />
+    <transition name="move-right">
+      <Nav v-if="!hidden" />
+    </transition>
     <img :src="logo" class="logo" />
-    <fa icon="shopping-bag" class="shopping" />
+    <a href="#"><fa icon="shopping-bag" class="shopping" /></a>
   </header>
 </template>
 
 <script>
+import Nav from "./Nav.vue";
 export default {
   name: "Header",
+  components: {
+    Nav,
+  },
   data() {
     return {
       logo: require("../assets/logo.svg"),
       hidden: true,
+      icon: "bars",
     };
+  },
+  methods: {
+    closeMenu() {
+      this.hidden = !this.hidden;
+      if (this.hidden == false) {
+        document.getElementsByClassName("menu")[0].style.transform =
+          "translateX(7.5em)";
+        this.icon = "times";
+      } else {
+        document.getElementsByClassName("menu")[0].style.transform =
+          "translateX(0)";
+        this.icon = "bars";
+      }
+    },
   },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 @import "../assets/_helpers";
 header {
   border-bottom: hsla(0, 0%, 0, 0.3) solid 1px;
@@ -55,51 +49,29 @@ header {
   justify-items: center;
   align-items: center;
 }
-nav {
-  position: fixed;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  z-index: 1;
-  background-color: hsl(0, 0%, 95%);
-  padding: 1em 1em;
-  border-right: rgba(#000, 0.5) solid 1px;
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-  justify-items: start;
-  gap: 2em;
-  min-width: 15em;
-  font-weight: 400;
-  & > ul {
-    display: flex;
-    flex-direction: column;
-    gap: 1.5em;
-    width: 100%;
-    a {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    }
-  }
-}
-
-.social-media {
-  display: flex;
-  font-size: 2rem;
-  justify-content: space-around;
-  width: 100%;
-}
-.menu {
-  font-size: 1.5rem;
-  cursor: pointer;
-}
-.close {
-  justify-self: end;
-}
 .logo {
   width: 2.5rem;
 }
 .shopping {
   font-size: 1.5rem;
+}
+.menu {
+  font-size: 1.5rem;
+  cursor: pointer;
+  z-index: 2;
+  transition: 0.5s ease;
+}
+
+.move-right-enter-from {
+  transform: translateX(-100%);
+}
+
+.move-right-enter-active,
+.move-right-leave-active {
+  transition: all 0.5s ease;
+}
+
+.move-right-leave-to {
+  transform: translateX(-100%);
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,74 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <a href="#">Jackets<fa icon="chevron-right" /></a>
+      </li>
+      <li>
+        <a href="#">Sneakers<fa icon="chevron-right" /></a>
+      </li>
+      <li>
+        <a href="#">Hoods<fa icon="chevron-right" /></a>
+      </li>
+      <li>
+        <a href="#">Hoods lorem<fa icon="chevron-right" /></a>
+      </li>
+      <li>
+        <a href="#">Hoods<fa icon="chevron-right" /></a>
+      </li>
+      <li>
+        <a href="#">Hoods<fa icon="chevron-right" /></a>
+      </li>
+    </ul>
+    <div class="social-media">
+      <a href="#"><fa :icon="['fab', 'instagram']" /></a>
+      <a href="#"><fa :icon="['fab', 'facebook']" /></a>
+      <a href="#"><fa :icon="['fab', 'twitter']" /></a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 1;
+  background-color: hsl(0, 0%, 95%);
+  padding: 1em 1em;
+  border-right: rgba(#000, 0.5) solid 1px;
+  display: grid;
+  grid-template-rows: 1.5em 1fr auto;
+  justify-items: start;
+  gap: 2em;
+  min-width: 15em;
+  font-weight: 400;
+  & > ul {
+    display: flex;
+    flex-direction: column;
+    gap: 1.5em;
+    width: 100%;
+    grid-row: 2/3;
+    a {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+}
+
+.social-media {
+  display: flex;
+  font-size: 2rem;
+  justify-content: space-around;
+  width: 100%;
+  grid-row: 3/4;
+}
+</style>
